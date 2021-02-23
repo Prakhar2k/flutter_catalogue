b0b341659da3af18d95c8353e75f75049b484a94
Prakhar <prakhargupta2k@gmail.com>
2021-02-23 21:25:03 +0530

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,17 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 001,
+      name: "iPhone 12 Pro Max",
+      desc: "Apple iPhone 12",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://www.gizmochina.com/wp-content/uploads/2020/05/iphone-12-pro-max-family-hero-all.jpg",
+    ),
+  ];
+}
+
 class Item {
   final num id;
   final String name;
@@ -8,15 +22,3 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
-
-final products = [
-  Item(
-    id: 001,
-    name: "iPhone 12 Pro Max",
-    desc: "Apple iPhone 12",
-    price: 999,
-    color: "#33505a",
-    image:
-        "https://www.gizmochina.com/wp-content/uploads/2020/05/iphone-12-pro-max-family-hero-all.jpg",
-  )
-];
