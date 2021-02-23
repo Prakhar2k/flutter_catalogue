073890f98be8339b30b29b84dd3c2169119e1c5b
Prakhar <prakhargupta2k@gmail.com>
2021-02-23 20:27:09 +0530

    day10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item {
+  final num id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  Item(
+    id: 001,
+    name: "iPhone 12 Pro Max",
+    desc: "Apple iPhone 12",
+    price: 999,
+    color: "#33505a",
+    image:
+        "https://www.gizmochina.com/wp-content/uploads/2020/05/iphone-12-pro-max-family-hero-all.jpg",
+  )
+];
